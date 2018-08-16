215d94bb1c959407d66727d47f992dc17c1b78fb
kostya_m <felix-mazur@mail.ru>
2018-08-17 00:02:12 +0300

    new

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,7 +14,13 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-UserAgent.set('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246');
+
+
+
+cordova.plugins.UserAgent.set('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246');
+
+
+
 
 
 
