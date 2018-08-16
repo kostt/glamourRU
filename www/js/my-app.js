1bc2dc0fcdb3d9be0799d939dd7188f1b5e6766e
kostya_m <felix-mazur@mail.ru>
2018-08-17 00:25:04 +0300

    new

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -16,10 +16,24 @@ var mainView = myApp.addView('.view-main', {
 
 
 
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
 
-window.navigator.UserAgent.set('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246');
+// device APIs are available
+//
+function onDeviceReady() {
 
-window.open('http://apache.org', '_blank', 'location=yes');
+    window.navigator.UserAgent.set('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246');
+
+    window.open('http://apache.org', '_blank', 'location=yes');
+
+}
+
+
+
+
+
 
 
 
