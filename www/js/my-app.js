3960406fbab8ea8a18eaaf01adf09e22fc8832b6
kostya_m <felix-mazur@mail.ru>
2018-08-16 23:30:09 +0300

    new

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,6 +14,8 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+UserAgent.set('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246');
+
 
 
 
